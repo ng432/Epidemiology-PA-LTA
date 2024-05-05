0d3d07e57c6e7fc0402a107f8933313c1b4f8466
Nicholas Gregory <ng432@cam.ac.uk>
2024-05-05 18:01:18 +0100

    organise file

--- Regression with multiple imputation/sixteenModelAnalysis_withMI.R
+++ Regression with multiple imputation/sixteenModelAnalysis_withMI.R
@@ -1,11 +1,12 @@
 
-
+library(abind)
 library(dplyr)
 library(nnet)
 library(mice)
 library(ggplot2)
 
 source("convergencePlotFunctions.R")
+source("predictedProbFunctions.R")
 
 load(file = "Imputed data/imputed_data_all_variables_rfr_m5iter100.Rdata")
 
@@ -18,7 +19,6 @@ load("../Data/data_for_regression.Rdata")
 
 # converting imputed data to table format to rename levels
 imputed_data = complete(imputed_data, action='long', include=TRUE)
-
 
 SEP_names = c(
   "Continued Education",
@@ -69,6 +69,20 @@ sixteen_focused_summary = summary(sixteen_focused_pool)
 
 
 
+
+############### No co-variate model  #############
+
+ncv_model = with(imputed_data,
+                             multinom(PA ~ 
+                                        SEP))
+
+ncv_fit = averageAICdivergence(ncv_model)
+
+ncv_pool = pool(ncv_model)
+
+ncv_summary = summary(ncv_pool)
+
+
 ############ Extracting predicted probabilites given SEP ###########
 
 # want to find predicted probability, assuming the average for continuous variables, and 
@@ -90,52 +104,8 @@ SEP_names <- c(
   "SEPEconomically Inactive"
 )
 
-calculate_pred_prob <- function(activity_level) {
-  # Filter the coefficients for the given activity level
-  ha_coeff <- sixteen_focused_summary %>%
-    filter(y.level == activity_level)
-  
-  # Calculate means
-  mean_malaise_16 <- mean(data_for_regression$malaise_16, na.rm = TRUE)
-  mean_children_26 <- mean(data_for_regression$children_26, na.rm = TRUE)
-  
-  # Coefficients names
-  gender_coeff_name <- 'genderFemale'
-  region_16_coeff_name <- 'region_16South East'
-  pc_10_coeff_name <- 'parental_class_10III non-manual'
-  
-  # Initialize pred_prob_list
-  pred_prob_list <- c()
-  
-  for (SEP in SEP_names) {
-    # initiating with intercept
-    log_odds <- ha_coeff[ha_coeff$term == "(Intercept)", 'estimate']
-    
-    if (SEP != "SEPContinued Education") {
-      log_odds <- log_odds + ha_coeff[ha_coeff$term == SEP, 'estimate']
-    }
-    
-    # adding odds for categorical variables which aren't the reference
-    log_odds <- log_odds + ha_coeff[ha_coeff$term == gender_coeff_name, 'estimate']
-    log_odds <- log_odds + ha_coeff[ha_coeff$term == region_16_coeff_name, 'estimate']
-    log_odds <- log_odds + ha_coeff[ha_coeff$term == pc_10_coeff_name, 'estimate']
-    
-    # adding odds for mean values for continuous variables
-    log_odds <- log_odds + mean_malaise_16 * ha_coeff[ha_coeff$term == "malaise_16", 'estimate']
-    log_odds <- log_odds + mean_children_26 * ha_coeff[ha_coeff$term == "children_26", 'estimate']
-    
-    # Calculate predicted probability
-    pred_prob <- exp(log_odds) / (1 + exp(log_odds))
-    pred_prob_list <- c(pred_prob_list, pred_prob)
-  }
-  
-  SEP_values <- factor(c(1:6), levels = 1:6, labels = SEP_names)
-  
-  pred_prob_df <- data.frame(PredProb = pred_prob_list, SEP = SEP_values)
-  
-  return(pred_prob_df)
-}
 
+# NOTE: 'Mid active' is the reference outcome for the log odds
 
 activity_names = c("Highly active",
                    "Mid active",
@@ -143,42 +113,150 @@ activity_names = c("Highly active",
                    "Decl. activity")
 
 
-# data frame to store predicted probabilites
+# data frame to store predicted probabilities and log odds
 pred_probalities <- data.frame(
   PredictiveProbability = numeric(),
+  LogOdds = numeric(),
   SEP = factor(levels = SEP_names),
   ActivityLevel = factor(levels = activity_names)
 )
 
-# calculating predicted probability for each of the non-reference classes 
+# calculating log odds for each of the non-reference classes, for each SEP 
+# adds placeholder value of 0 for predictive probabilities
 for (activity in activity_names)
   {
+  # if activity level is not reference activity level, calculate log odds
   if (activity != 'Mid active')
   {
-  activity_pp = calculate_pred_prob(activity)
+    log_odds = calculate_log_odds(activity, sixteen_focused_summary, data_for_regression)
+  }
+  else # i.e. if activity level is reference, need to set log odds = 0
+  {
+    log_odds = rep(0, times = 6)
+  }
   
-  new_data <- data.frame(PredictiveProbability = activity_pp$PredProb,
-                         SEP = factor(activity_pp$SEP,  levels = SEP_names),
+  str(log_odds)
+  
+  new_data <- data.frame(PredictiveProbability = rep(0, times = 6),
+                         LogOdds = log_odds,
+                         SEP = factor(SEP_names, levels = SEP_names),
                          ActivityLevel = factor(rep(activity, times = 6), levels = activity_names))
   
   pred_probalities <- rbind(pred_probalities, new_data)
-  }
 }
 
-# calculating probability for reference class, for each SEP
+
+
+# calculating probability each PA / outcome, for each SEP / predictor
+# this is assuming that predicted probabilities result from a softmax function of log odds
+# note, log odds for reference PA is 1
+
 for (SEP in SEP_names) 
 {
-  # probability for reference class (i.e. remaining probability)
-  prob = 1 - sum(pred_probalities[pred_probalities$SEP == SEP, "PredictiveProbability"])
+  # denominator for softmax is sum of log odds
+  denominator = sum(exp(pred_probalities[pred_probalities$SEP == SEP, "LogOdds"]))
   
-  new_row = data.frame(PredictiveProbability = prob,
-                       SEP = factor(SEP,  levels = SEP_names),
-                       ActivityLevel = factor('Mid active', levels = activity_names))
-  
-  pred_probalities <- rbind(pred_probalities, new_row)
+  for (activity in activity_names)
+  {
+    numerator <- pred_probalities[pred_probalities$SEP == SEP & pred_probalities$ActivityLevel == activity, "LogOdds"]
+    numerator = exp(numerator)
+    
+    pred_prob = numerator / denominator
+    
+    # setting pred_probability 
+    pred_probalities$PredictiveProbability[
+      pred_probalities$SEP == SEP 
+      & pred_probalities$ActivityLevel == activity] <- pred_prob
+    
+  }
   
 }
 
+
+
+
+
+
+############ Bootstrapping for CI across multiple imputed dataset ###########
+
+## Using method 1 for bootstrapping with MI from https://www.ncbi.nlm.nih.gov/pmc/articles/PMC5986623/
+
+# STEP 1: combining multiple imputed datasets into one single dataset for bootstrapping
+
+# loop through each imputed data set (1 to 5)
+
+
+variable_list = colnames(imputed_data$data)
+
+# takes the dataset with it's missing values 
+dataset_to_fill = imputed_data$data
+
+filled_datasets = list()
+
+for (i in 1:imputed_data$m) {
+  # form each completed column
+  j = 1
+  for (variable in variable_list)
+  {
+    missing_elements = imputed_data$where[,j]
+    imputed_values = imputed_data$imp[[variable_list[j]]][,i]
+    dataset_to_fill[[variable]][missing_elements] = imputed_values
+    j = j + 1
+  }
+  
+  filled_datasets[[i]] = dataset_to_fill
+}
+
+# good news... modelling this gets similar results to pooling from separate instances
+all_imputed_datasets = bind_rows(filled_datasets, .id = "iteration")
+
+pred_probs_from_pool = predicted_prob_estimate(all_imputed_datasets)
+
+###  all samples pooled
+### statistic we are looking at it is predicted probability
+### need to return a function for predicted probability
+
+
+boot_attempt = boot(all_imputed_datasets, predicted_prob_estimate, 1)
+
+
+## For each imputed data set, carrying out bootstrapping
+
+pp_boot_list = list()
+
+dataset_to_fill = imputed_data$data
+for (i in 1:imputed_data$m) {
+  # form each completed column
+  j = 1
+  for (variable in variable_list)
+  {
+    missing_elements = imputed_data$where[,j]
+    imputed_values = imputed_data$imp[[variable_list[j]]][,i]
+    dataset_to_fill[[variable]][missing_elements] = imputed_values
+    j = j + 1
+  }
+  
+  booted_pp = boot(dataset_to_fill, predicted_prob_estimate, 500)
+  
+  pp_boot_list[[i]] = booted_pp
+}
+
+
+all_t_matrices <- do.call(rbind, lapply(pp_boot_list, function(boot) boot$t))
+
+ci_lower <- apply(all_t_matrices, 2, quantile, probs = 0.025)
+ci_upper <- apply(all_t_matrices, 2, quantile, probs = 0.975)
+
+pred_probalities$ci_lower = ci_lower
+pred_probalities$ci_upper = ci_upper
+
+
+
+
+
+################ Outputting pretty graph ##########################
+
+# just renaming to be clear for graph
 SEP_names = c(
   "Cont. Educ.",
   "Managerial",
@@ -191,12 +269,17 @@ SEP_names = c(
 # renaming levels to make more sense
 levels(pred_probalities$SEP) = SEP_names
 
-ggplot(pred_probalities, aes(x = ActivityLevel, y = PredictiveProbability, color = SEP)) +
+ggplot(pred_probalities, aes(x = SEP, y = PredictiveProbability, color = ActivityLevel)) +
   geom_point() +
   theme_minimal() +
   labs(title = "Scatter Plot", x = "SEP", y = "Probability", color = "Activity Level")
 
 
+ggplot(pred_probalities, aes(x = SEP, y = PredictiveProbability, color = ActivityLevel)) +
+  geom_point() +  # Plot points
+  geom_errorbar(aes(ymin = ci_lower, ymax = ci_upper), width = 0.2) +  # Add error bars for CI
+  theme_minimal() +
+  labs(title = "Scatter Plot with Confidence Intervals", x = "SEP", y = "Predictive Probability", color = "Activity Level")
 
 
 
